4234b96c945c6b947cb8f2c300c1b0d4f2d505ab
RomanBits <roman.bits@testview.co.il>
2025-03-28 12:11:03 +0300

    Initial commit

---
+++ LeaveManagementSystem.web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.web.Controllers;
+
+public class TestController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        var data = new TestViewModel
+        {
+            Name = "Test2",
+            DateOfBirth = new DateTime(1956,5, 23)
+        };
+        return View(data);
+    }
+}
