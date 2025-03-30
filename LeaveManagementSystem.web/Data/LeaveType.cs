a28f6ba8cf3b3d9da718a72f293bf109800b9c97
Roman Bits <romanbits2@gmail.com>
2025-03-30 17:56:28 +0300

    complited leave managment module

---
+++ LeaveManagementSystem.web/Data/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.web.Data;
+
+public class LeaveType
+{
+    public int Id { get; set; }
+    [StringLength(150)]
+    public string Name { get; set; }
+    public int NumberOfDays { get; set; }
+}
--- LeaveManagementSystem.web/Data/Migrations/20250330131945_AddingLeaveTypesTable.Designer.cs
+++ LeaveManagementSystem.web/Data/Migrations/20250330131945_AddingLeaveTypesTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LeaveManagementSystem.web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LeaveManagementSystem.web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250330131945_AddingLeaveTypesTable")]
+    partial class AddingLeaveTypesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.2");
